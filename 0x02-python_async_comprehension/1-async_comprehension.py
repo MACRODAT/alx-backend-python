5b02d987eb943dc1adba5e0a44e2d04f30e4d71a
MACRODAT <macrodatservices@gmail.com>
2024-08-18 09:54:21 +0100

    Changes to the file

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,14 +2,12 @@
 
 """ Async Comprehensions """
 
-from asyncio import sleep
-from random import uniform
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """ Async Comprehensions  """
+    """ Comprehennsion  """
     a = [i async for i in async_generator()]
     return a
